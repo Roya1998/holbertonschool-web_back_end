1aa288134693f103cd8a20b1b276bdfc61af7524
ROYA YUSIFBAYLI <109675069+Roya1998@users.noreply.github.com>
2025-12-01 17:43:34 +0400

    all tasks added

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -15,4 +15,3 @@ async_generator = __import__('0-async_generator').async_generator
 async def async_comprehension() -> List[float]:
     ''' Return list of values yielded by async_generator. '''
     return [value async for value in async_generator()]
-
