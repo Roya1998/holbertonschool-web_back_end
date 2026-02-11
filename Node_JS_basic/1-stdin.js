7aa230ce2e1bb42a476c20009fa9d1fd7c42334a
ROYA YUSIFBAYLI <royayusifbayli@gmail.com>
2026-02-11 23:02:02 +0400

    2nd task corrected

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,13 +2,11 @@ process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.setEncoding("utf8");
 
-process.stdin.on("readable", () => {
-  const chunk = process.stdin.read();
-  if (chunk !== null) {
-    process.stdout.write(`Your name is: ${chunk}`);
-  }
+process.stdin.on("data", (input) => {
+  const name = input.trim();
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing\n");
+  console.log("This important software is now closing");
 });
