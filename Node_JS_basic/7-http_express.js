68670d79113a84904941ec3126789d416832df08
ROYA YUSIFBAYLI <royayusifbayli@gmail.com>
2026-02-12 01:58:17 +0400

    some codes changed

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,31 +1,59 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
+
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
+
+    const students = lines.slice(1); // Skip header
+    const fields = {};
+
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (firstname && field) {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+    });
+
+    let output = `Number of students: ${students.length}\n`;
+
+    for (const [field, names] of Object.entries(fields)) {
+      output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+    }
+
+    return output.trim();
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
 
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
 app.get('/students', async (req, res) => {
-  const dbpath = process.argv[2];
-  let o = 'This is the list of our students\n';
-  if (!dbpath) {
-    o += 'Cannot load the database';
-    res.status(500).send(o);
-    return;
-  }
+  const databasePath = process.argv[2];
+
   try {
-    const studentsData = await countStudents(dbpath);
-    o += studentsData;
-    res.send(o);
-  } catch (error) {
-    o += 'Cannot load the database';
-    res.send(o);
+    const studentsList = await countStudents(databasePath);
+    res.send(`This is the list of our students\n${studentsList}`);
+  } catch (err) {
+    res.send('This is the list of our students\nCannot load the database');
   }
 });
 
 app.listen(1245, () => {
-  console.log('Express server is listening on port 1245');
+  console.log('Server listening on port 1245');
 });
 
 module.exports = app;
