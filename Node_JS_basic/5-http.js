c5274efdbd0008a4e1937445eede5a975e0aa5e5
ROYA YUSIFBAYLI <royayusifbayli@gmail.com>
2026-02-12 00:49:50 +0400

    all tasks corrected

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,36 +1,31 @@
-const fs = require('fs');
+const http = require('http');
+const countStudents = require('./3-read_file_async');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
+const database = process.argv[2];
 
-      const lines = data.split('\n').filter((line) => line.trim() !== '');
-      const students = lines.slice(1);
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
 
-      const fields = {};
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.statusCode = 200;
+    res.write('This is the list of our students\n');
 
-      students.forEach((student) => {
-        const parts = student.split(',');
-        const firstname = parts[0];
-        const field = parts[3];
-
-        if (!fields[field]) fields[field] = [];
-        fields[field].push(firstname);
+    countStudents(database)
+      .then((data) => {
+        res.end(data);
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
       });
+  } else {
+    res.statusCode = 404;
+    res.end('Not Found');
+  }
+});
 
-      let result = `Number of students: ${students.length}\n`;
+app.listen(1245);
 
-      Object.keys(fields).forEach((field) => {
-        result += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
-      });
-
-      resolve(result.trim());
-    });
-  });
-}
-
-module.exports = countStudents;
+module.exports = app;
