482bc2fd5e279ee3170633f7505ea0c737ed3430
ROYA YUSIFBAYLI <royayusifbayli@gmail.com>
2026-02-12 01:39:11 +0400

    package json added

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,39 +1,63 @@
 const http = require('http');
-const url = require('url');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs').promises;
 
-const app = http.createServer((req, res) => {
-  const reqUrl = url.parse(req.url, true);
+async function countStudents(path) {
+  try {
+    const data = await fs.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    
+    if (lines.length <= 1) {
+      throw new Error('Cannot load the database');
+    }
 
-  if (reqUrl.pathname === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const students = lines.slice(1); // Skip header
+    const fields = {};
+
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (firstname && field) {
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(firstname);
+      }
+    });
+
+    let output = `Number of students: ${students.length}\n`;
+    
+    for (const [field, names] of Object.entries(fields)) {
+      output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+    }
+
+    return output.trim();
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
     res.end('Hello Holberton School!');
-  } else if (reqUrl.pathname === '/students') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.write('This is the list of our students\n');
-
+  } else if (req.url === '/students') {
     const databasePath = process.argv[2];
-
-    if (!databasePath) {
-      res.end('Cannot load the database');
-    } else {
-      countStudents(databasePath)
-        .then((output) => {
-          res.write(output);
-          res.end();
-        })
-        .catch(() => {
-          res.end('Cannot load the database');
-        });
+    
+    try {
+      const studentsList = await countStudents(databasePath);
+      res.end(`This is the list of our students\n${studentsList}`);
+    } catch (err) {
+      res.end('This is the list of our students\nCannot load the database');
     }
   } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.statusCode = 404;
     res.end('Not Found');
   }
 });
 
 app.listen(1245, () => {
-  console.log('Server is listening on port 1245');
+  console.log('Server listening on port 1245');
 });
 
 module.exports = app;
